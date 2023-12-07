546f4413f60bca9b55ed3ca3118985b461901f4d
thaimore <volodea.leyciu@gmail.com>
2023-12-07 19:01:41 +0200

    Memory leak

--- src/main/java/com/org/ds/messaging/MessageRoute.java
+++ src/main/java/com/org/ds/messaging/MessageRoute.java
@@ -1,20 +1,25 @@
 package com.org.ds.messaging;
 
 import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import static com.org.ma.utils.Constants.DELIVERY_CHANNEL;
 
 @Component
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class MessageRoute extends RouteBuilder {
+
+    @Value(value = "${spring.kafka.consumer.bootstrap-servers}")
+    private String brokerAddress;
 
     private MessageHandler handler;
 
     @Override
     public void configure() {
-        from("kafka:%s?brokers=172.18.0.1:9092".formatted(DELIVERY_CHANNEL))
+        from("kafka:%s?brokers=%s".formatted(DELIVERY_CHANNEL, brokerAddress))
                 .process(handler);
     }
 }
